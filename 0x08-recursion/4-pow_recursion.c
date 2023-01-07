03e46561f6def6db6c193e2bedcc56bae6487c44
Okunsebor <joelosehioria@gmail.com>
2023-01-07 00:50:13 -0800

    My tasks

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,13 +13,11 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
 
-	if (y == 1)
+	else
 	{
-		return (_pow_recursion(x, y + 1) / x);
+		return (1);
 	}
-
-	return (_pow_recursion(x, y - 1) * x);
 }
-
-
