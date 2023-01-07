8be48baf42bc3e94ac977a20ad1fef84e198e43a
Okunsebor <joelosehioria@gmail.com>
2023-01-07 14:38:56 -0800

    What is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print program's name
+ * @argc: argument count
+ * @argv: argument vector
+ * Owmed by Okunsebor
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("Program's name is : %s\n", argv[0]);
+	return (0);
+}
