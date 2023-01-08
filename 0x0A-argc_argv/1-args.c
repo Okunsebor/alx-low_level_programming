e8f1323d3a594b0d2e7ad8ea3694455e82fdad64
Okunsebor <joelosehioria@gmail.com>
2023-01-08 09:51:43 -0800

    print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of argument
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int counter;
+
+	for (counter = 0; counter < argc; counter++)
+
+		printf("argv[%2d]: %s\n", counter, argv[counter]);
+
+	return (0);
+}
